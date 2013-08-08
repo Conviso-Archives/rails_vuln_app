07b44847f19f331db3edee8942233025add92f0b
Rick <ricks2600@gmail.com>
2013-08-08 14:45:49 -0300

    Creating the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsVulnApp::Application.config.secret_token = 'b85431cdab5fd04d25b9c6e4cf5bf1ac171ba3d2689fe38e186bf8cc2882c435d0f5c7657afd9b9c8806ac351499f803e07de8dca03341af7ad1c990a9ed3c8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsVulnApp::Application.config.session_store :cookie_store, key: '_rails_vuln_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsVulnApp::Application.config.session_store :active_record_store
